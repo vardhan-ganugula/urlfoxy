ef2e3c90793ed4471d99a511e7b4e17f37b779ad
vardhan ganugula <gvardhan2004@gmail.com>
2025-07-04 20:55:08 +0530

    add forgot password page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
+import ForgotPasswordPage from './pages/ForgotPasswordPage'
 const App = () => {
   return (
     <>
@@ -10,6 +11,7 @@ const App = () => {
           <Route path='/' element={<Homepage />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<SignupPage />} />
+          <Route path='/forgot-password' element={<ForgotPasswordPage />} />
       </Routes>
     </>
   )
---
+++ client/src/pages/ForgotPasswordPage.jsx
@@ -0,0 +1,108 @@
+import HomepageHeader from "../components/HomepageHeader";
+import Footer from "../components/Footer";
+import UCat from "../assets/imgs/uclip-cat.webp";
+import { MdOutlineMail } from "react-icons/md";
+import { PiPassword } from "react-icons/pi";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { useCallback, useState } from "react";
+import { Link } from "react-router-dom";
+import {useForm} from 'react-hook-form'
+import {zodResolver} from '@hookform/resolvers/zod'
+import { loginSchema } from "../schemas/auth.schema";
+import {z} from 'zod';
+
+
+const ForgotPasswordPage = () => {
+  const [isVisiblePassword, setVisiblePassword] = useState(false);
+  const forgotPasswordSchema = z.object({
+    email: z.string().email({
+        message: 'Invalid Email'
+        
+    })
+  })
+  const {register, handleSubmit, formState: {errors, isSubmitting}} = useForm({
+    resolver : zodResolver(forgotPasswordSchema)
+  })
+  const handleLogin = useCallback(async (data) => {
+    await new Promise((result, reject) => {
+      setTimeout(() => {
+        result('hi')
+      }, 5000)
+    })
+    console.log(data)
+  },[])
+  console.log(isSubmitting)
+  return (
+    <>
+      <HomepageHeader />
+      <main className=" md:pb-[300px] h-[80vh]  md:h-[110vh] bg-black w-full md:pt-24 ">
+        <div className="md:w-[90%] lg:w-[70%] mx-auto h-full bg-zinc-950 border border-zinc-900 flex items-center md:flex-row flex-col w-full gap-4 justify-start rounded-lg p-2 overflow-hidden ">
+          <div className="hidden md:block md:w-1/2 w-full shrink-0 rounded-lg bg-zinc-900 md:h-full p-10 ">
+            <h2 className="text-white text-5xl font-bold">
+              Login to <span className="text-blue-600">UCLIP</span>
+            </h2>
+            <div className="w-2/3 h-2/3 mx-auto mt-18">
+              <img
+                src={UCat}
+                alt="uclip login page"
+                className="w-full h-full object-contain hue-rotate-30"
+              />
+            </div>
+          </div>
+          <div className="md:w-1/2 w-full md:h-full relative">
+            <div className="absolute -right-3 -top-3 bg-blue-400 opacity-45 h-[340px] w-[340px] rounded-full blur-[90px]"></div>
+            <div className="rounded-lg absolute top-0 left-0 backdrop-blur-[200px] h-full w-full text-white">
+              <div className="w-2/3 mx-auto mt-28">
+                <h4 className="font-bold text-3xl uppercase">Welcome Back</h4>
+                <div className="">
+                  <form className="flex w-full flex-col mt-18 gap-2"
+                    onSubmit={handleSubmit(handleLogin)}
+                  >
+                    <div className="w-full inline-flex bg-white/5 px-5 py-3 rounded-md">
+                      <label htmlFor="email" className="w-8 py-2">
+                        <MdOutlineMail />
+                      </label>
+                      <input
+                        type="text"
+                        className="inline-block grow outline-none ring-0 text-white bg-transparent focus:bg-transparent autofill:text-white autofill:bg-transparent text-sm placeholder:tracking-wider"
+                        placeholder="your email address"
+                        autoComplete="email"
+                        id="email"
+
+                        {
+                          ...register('email')
+                        }
+                      />
+                    </div>
+                    <p className="text-red-500">{
+                        errors.email && errors.email.message
+                      }</p>
+                    <button
+                      disabled={isSubmitting}
+                      className={`rounded px-2 w-28 text-sm py-3 mt-2 hover:bg-blue-700 text-white  ${isSubmitting ? 'cursor-not-allowed bg-blue-900' : ' bg-blue-600 cursor-pointer'} disabled:bg-blue-900`}
+                      type="submit"
+                    >
+                      Send Email
+                    </button>
+                  </form>
+                </div>
+                <div className="mt-14 text-zinc-300 text-sm flex justify-between">
+                  <div>
+                    don't you have account?{" "}
+                    <Link to="/register" className="text-sky-400">
+                      Register
+                    </Link>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default ForgotPasswordPage;
