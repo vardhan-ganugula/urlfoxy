7e372399019d4ed4f58d28a2c851ae8ef38a5afc
vardhan ganugula <gvardhan2004@gmail.com>
2025-07-09 21:45:12 +0530

    integrate layout with rtk

--- client/src/layouts/DefaultLayout.jsx
+++ client/src/layouts/DefaultLayout.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import Footer from "../components/Footer";
 import HomepageHeader from "../components/HomepageHeader";
+import {useGetUserProfileQuery} from '../store/apis/index.js';
+import { Navigate, useLocation } from 'react-router-dom';
+import { publicRoutes, protectedRoutes } from "../constants/routeLinks.js";
 
 const DefaultLayout = ({ children }) => {
+    const {pathname} = useLocation();
+    const {isSuccess, isError} = useGetUserProfileQuery();
+    if(isError){
+      if(protectedRoutes.includes(pathname)){
+        return <Navigate to='/login' />
+      }
+    }
+    if(isSuccess){
+      if(!publicRoutes.includes(pathname) && (!protectedRoutes.includes(pathname))){
+        return <Navigate to='/dashboard' />
+      }
+    }
   return (
     <>
       <HomepageHeader />
--- client/src/store/apis/index.js
+++ client/src/store/apis/index.js
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_API_URL,
+    credentials: 'include'
   }),
   endpoints: function (builder) {
     return {
