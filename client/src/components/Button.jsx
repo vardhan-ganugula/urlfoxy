84e1117aacb23d53806bc6ad54906540d7f5bb77
Vardhan Ganugula <gvardhan2004@gmail.com>
2025-07-31 23:47:06 +0530

    update button

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,31 +1,32 @@
 import React from "react";
 
+const baseStyles = {
+  default: "bg-primary text-black rounded-full",
+  outline: "bg-black border-2 border-primary text-primary rounded-full",
+  glowy: "rounded-full glowy",
+  squared: "bg-primary text-black rounded",
+};
+
 const Button = ({
   variant = "default",
   children,
-  bolded = "false",
+  bolded = false,
   onClick,
-  className,
-  disabled = false
+  className = "",
+  disabled = false,
 }) => {
+  const variantClass = baseStyles[variant] || "";
+
   return (
     <button
       onClick={onClick}
       disabled={disabled}
       className={`
-         cursor-pointer p-[2px] hover:bg-primary/80 md:text-md text-xs disabled:bg-primary/80 disabled:cursor-not-allowed ${
-           variant === "default"
-             ? "bg-primary text-black rounded-full"
-             : variant === "outline"
-             ? "bg-black border-2 border-primary text-primary rounded-full"
-             : variant === "glowy"
-             ? "rounded-full glowy "
-             : variant == "squared"
-             ? "bg-primary text-black rounded"
-             : ""
-         }
-        ${bolded && "font-bold"} ${className}
-    `}
+        cursor-pointer p-[2px] hover:bg-primary/80 md:text-md text-xs
+        disabled:bg-primary/80 disabled:cursor-not-allowed
+        ${bolded ? "font-bold" : ""}
+        ${variantClass} ${className}
+      `}
     >
       <span
         className={`px-5 py-2 inline-flex w-full h-full ${
