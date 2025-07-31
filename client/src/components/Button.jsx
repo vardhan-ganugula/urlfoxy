a2cf36211f8b9bc6932c35167a2db3cfae23971e
Vardhan Ganugula <gvardhan2004@gmail.com>
2025-07-31 22:24:48 +0530

    update domain

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -5,26 +5,37 @@ const Button = ({
   children,
   bolded = "false",
   onClick,
-  className
+  className,
+  disabled = false
 }) => {
   return (
     <button
       onClick={onClick}
+      disabled={disabled}
       className={`
-         cursor-pointer p-[2px] hover:bg-primary/80 md:text-md text-xs ${
-          variant === "default"
-            ? "bg-[#A1E231] text-black rounded-full"
-            : variant === "outline"
-            ? "bg-black border-2 border-[#A1E231] text-[#A1E231] rounded-full"
-            : variant === "glowy"
-            ? "rounded-full glowy "
-            : ""
-        }
+         cursor-pointer p-[2px] hover:bg-primary/80 md:text-md text-xs disabled:bg-primary/80 disabled:cursor-not-allowed ${
+           variant === "default"
+             ? "bg-primary text-black rounded-full"
+             : variant === "outline"
+             ? "bg-black border-2 border-primary text-primary rounded-full"
+             : variant === "glowy"
+             ? "rounded-full glowy "
+             : variant == "squared"
+             ? "bg-primary text-black rounded"
+             : ""
+         }
         ${bolded && "font-bold"} ${className}
     `}
     >
       <span
-        className={`px-5 py-2 inline-flex w-full h-full ${variant === 'glowy' ? 'bg-gradient-to-bl from-slate-700 to-30% to-slate-800 text-white rounded-[inherit] blur-none z-[1]' : ''}`}>{children}</span>
+        className={`px-5 py-2 inline-flex w-full h-full ${
+          variant === "glowy"
+            ? "bg-gradient-to-bl from-slate-700 to-30% to-slate-800 text-white rounded-[inherit] blur-none z-[1]"
+            : ""
+        }`}
+      >
+        {children}
+      </span>
     </button>
   );
 };
