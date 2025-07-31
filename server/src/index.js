606d6793c6c6b4834b42710a0d11e5ff9f51b030
Vardhan Ganugula <gvardhan2004@gmail.com>
2025-07-31 13:55:22 +0530

    remove trush proxy

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,6 @@ config();
 connectDB();
 
 const app = express();
-app.set('trust proxy', true); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
